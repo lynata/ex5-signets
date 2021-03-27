5d157a6560668c163cadf7edb34803f81d005fa7
Lyna <lyly8888@hotmail.com>
2021-03-27 14:43:28 -0400

    debut exercice

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,14 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyDBkHd3rUW8dZ5D07SUpNkPG7yMxRuWbdM",
+  authDomain: "panier-achat-lt.firebaseapp.com",
+  databaseURL: "https://panier-achat-lt-default-rtdb.firebaseio.com",
+  projectId: "panier-achat-lt",
+  storageBucket: "panier-achat-lt.appspot.com",
+  messagingSenderId: "531000313975",
+  appId: "1:531000313975:web:b29c2de23e52c9b8791e72",
+  measurementId: "G-KLNJ2561WS" 
 };
 export default firebaseConfig;
 
